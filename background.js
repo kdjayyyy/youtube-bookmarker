08889e33ec2217bd674a3952574ba999507cbd06
kdjayyyy <thekdas1000@gmail.com>
2024-10-28 16:43:31 +0530

    main files pushed

---
+++ background.js
@@ -0,0 +1,16 @@
+// script for handling background tasks of the extension
+
+chrome.tabs.onUpdated.addListener((tabId, tab) => {
+  // if (tab.url && tab.url.includes('youtube.com/watch')) {
+  if (tab.url && tab.url.includes('youtube.com/watch')) {
+    const queryParameters = tab.url.split('?')[1];
+    const urlParameters = new URLSearchParams(queryParameters);
+
+    console.log(urlParameters);
+
+    chrome.tabs.sendMessage(tabId, {
+      type: 'NEW',
+      videoId: urlParameters.get('v'),
+    });
+  }
+});
